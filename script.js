9736bf1d910c843e7bd585ae06e377ea77a93e39
AfzGit <you@example.com>
2022-10-04 11:08:28 +0530

    etc

--- script.js
+++ script.js
@@ -1,16 +1,36 @@
-function calCheck() {
-    const cals = document.getElementById('total-cal').value;
-    const meta = document.getElementById('metabolism').value;
-    const sum = parseInt(cals) - parseInt(meta)
-    var calStatus = "None";
+// dynamic list
+let counter = 1;
+let totalCals = 0;
 
-    if (sum > 0) {
+function additem() {
+    var completelist = document.getElementById("thelist");
+
+    let consumables = document.getElementById("item").value;
+    let itemCals = document.getElementById("cals").value;
+
+    completelist.innerHTML += "<li>" + consumables +": " + itemCals + " Calories </li>";
+
+    counter++;
+    totalCals += parseInt(itemCals);
+}
+
+function calCheck() {
+    const meta = document.getElementById('metabolism').value;
+    let about = document.getElementById("about");
+
+    const net = totalCals - parseInt(meta)
+    let calStatus = "None";
+
+    if (net > 0) {
         calStatus = "You are not calorie deficit";
-    } else if (sum < 0) {
+    } else if (net < 0) {
         calStatus = "You are calorie deficit!";
     } else {
         calStatus = "calorie and metabolism are balanced";
     }
-    alert(calStatus + "... Net calories = " + sum + "" );
+
+
+    about.innerHTML =  "Total Calories: " + totalCals + " Calories<br>Metabolism: " + meta + " Calories.<br><br>Net Calories: " + net + " Calories<br><br>" + calStatus;
 }
+
 
