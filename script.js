193ab44eb0b9a73b2408855353511871b80a7299
AfzGit <you@example.com>
2022-10-04 11:38:34 +0530

    etc

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-// dynamic list
-let counter = 1;
+// one kg = 7000 cals
+const kgcal = 7000;
+// for list item 
 let totalCals = 0;
+// global variable days to calculate days
+let days = 0;
 
+// when + button is pressed
 function additem() {
     var completelist = document.getElementById("thelist");
 
@@ -10,27 +14,53 @@ function additem() {
 
     completelist.innerHTML += "<li>" + consumables +": " + itemCals + " Calories </li>";
 
-    counter++;
     totalCals += parseInt(itemCals);
 }
 
+// Calorie calculations when caclulate button is pressed
 function calCheck() {
     const meta = document.getElementById('metabolism').value;
     let about = document.getElementById("about");
 
-    const net = totalCals - parseInt(meta)
+    let net = totalCals - parseInt(meta)
     let calStatus = "None";
 
     if (net > 0) {
         calStatus = "You are not calorie deficit";
+        days = kgcal / net;
     } else if (net < 0) {
         calStatus = "You are calorie deficit!";
-    } else {
+        days = -(kgcal / net);
+    } else if (net == 0) {
         calStatus = "calorie and metabolism are balanced";
+    } else {
+        alert("Only numbers allowed in Metabolism and Calories fields");
+        totalCals = "error";
+        net = "error";
     }
 
+    if (net != "error") {
+        about.innerHTML =  "Total Calories: " + totalCals + " Calories<br>Metabolism: " + meta + " Calories.<br><br>Net Calories: " + net + " Calories<br><br>" + calStatus;
+    } else {
+        about.innerHTML = "Please enter all the field properly"
+    }
 
-    about.innerHTML =  "Total Calories: " + totalCals + " Calories<br>Metabolism: " + meta + " Calories.<br><br>Net Calories: " + net + " Calories<br><br>" + calStatus;
+    if (net < 0) {
+        about.innerHTML += "<br><br>You will lose 1 kg in " + Math.round(days) + " days if you keep this diet up.";
+    } else if (net > 0) {
+        about.innerHTML += "<br><br>You will gain 1 kg in " + Math.round(days) + " days if you keep this diet up.";
+    } else {
+        about.innerHTML += "<br><br>You will not lose weight in the coming days";
+    }
 }
 
+// when reset button is pressed
+function resetlist() {
+    let about = document.getElementById("about");
+    let completelist = document.getElementById("thelist");
 
+    completelist.innerHTML = "";
+        about.innerHTML = "Calculate your net calories here";
+
+    totalCals = 0;
+}
